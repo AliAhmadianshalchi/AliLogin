d49261839eb95a925ee99a1a46261887b12f75d9
Ali Ahmadian <40244903+AliAhmadianshalchi@users.noreply.github.com>
2021-07-27 20:55:59 -0300

    WIP - avances in project

---
+++ AliLogin/Controller/InsideViewController.swift
@@ -0,0 +1,20 @@
+//
+//  InsideViewController.swift
+//  AliLogin
+//
+//  Created by Ali Ahmadian shalchi on 27/07/2021.
+//
+
+import UIKit
+
+class InsideViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+
+
+}
--- AliLogin/Controller/LoginViewController.swift
+++ AliLogin/Controller/LoginViewController.swift
@@ -6,14 +6,47 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var registerButton: UIButton!
+    @IBOutlet weak var facebookLoginButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpLoginButton()
+        
+        if let token = AccessToken.current,
+                !token.isExpired {
+                // User is logged in, do work such as go to next view controller.
+            }
+    }
+    
+    func setUpLoginButton() {
+        let loginButton = FBLoginButton()
+        loginButton.center = view.center
+        loginButton.permissions = ["public_profile", "email"]
+        view.addSubview(loginButton)
     }
 
-
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func registerButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func loginWithFaceBookPressed(_ sender: UIButton) {
+        
+    }
+    
 }
 
